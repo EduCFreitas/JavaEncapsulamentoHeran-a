cf62fc743da02496593cc9d2cb5288dc654a3fcb
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-05-26 15:21:35 -0300

    Aula Java encapsulamento e herança

---
+++ AnimaisHeranca/src/model/Gato.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Gato extends Mamifero {
+	private boolean bigode;
+
+	public boolean isBigode() {
+		return bigode;
+	}
+
+	public void setBigode(boolean bigode) {
+		this.bigode = bigode;
+	}
+	
+	public void miar() {
+		System.out.println("O gato "+getNome()+" está miando!");
+	}
+}
